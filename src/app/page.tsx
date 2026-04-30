b8c004856536bbc95a22761eec141b9d495d0b7e
just-prem22 <premkumarjh231@gmail.com>
2026-04-30 12:15:47 +0530

    fix: remove invalid revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
+
 import { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 import { Orbitron } from "next/font/google";
